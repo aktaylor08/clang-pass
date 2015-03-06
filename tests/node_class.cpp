5207db8e5080430ac2dd08a1968fdd516a638a18
Adam Taylor <aktaylor08@gmail.com>
2015-03-06 15:25:32 -0600

    simplied node class to bare bones

--- tests/node_class.cpp
+++ tests/node_class.cpp
@@ -1,13 +1,5 @@
 #include <ros/ros.h>
-#include <stdio.h>
-#include <string>
-#include <cmath>
-#include <deque>
-#include <std_msgs/Float64.h>
 #include <std_msgs/Int16.h>
-#include <std_msgs/Bool.h>
-#include <std_msgs/String.h>
-
 
 using namespace std;
 
@@ -17,43 +9,29 @@ public:
 private:
 
   //Callbacks
-  void callbackSimple(const boost::shared_ptr<std_msgs::Float64 const> &msg_in);
+  void callbackSimple(const boost::shared_ptr<std_msgs::Int16 const> &msg_in);
   ros::NodeHandle 	private_nh_;
   ros::NodeHandle 	nh;
   int _threshold;
   ros::Subscriber 	msg_sub;
   ros::Publisher 	msg_pub;
-  ros::Publisher 	other_pub;
 };
 
 
 Test::Test(void): private_nh_("~"){
     private_nh_.param("thresh_val", _threshold, 42);
-	ROS_INFO("Kalman started");
-	msg_pub = nh.advertise<std_msgs::String>("i_like_turtles", 2);
-	other_pub = nh.advertise<std_msgs::String>("caar_name", 2);
+	msg_pub = nh.advertise<std_msgs::Int16>("publish_topic", 2);
 	msg_sub = nh.subscribe("give_me_info", 1, &Test::callbackSimple, this);
-
 }
 
 
-void Test::callbackSimple(const boost::shared_ptr<std_msgs::Float64 const> &in_msg)
+void Test::callbackSimple(const boost::shared_ptr<std_msgs::Int16 const> &in_msg)
 {
          if(in_msg-> data > _threshold){
-            std_msgs::String msg;
-
-            std::stringstream ss;
-            ss << "hello world " << in_msg;
-            msg.data = ss.str();
-             msg_pub.publish(msg);
+            std_msgs::Int16 msg;
+            msg.data = 42;
+            msg_pub.publish(msg);
          }
-         std_msgs::String mmmm;
-
-         std::stringstream ss;
-         ss << "cargs";
-         mmmm.data = ss.str();
-         other_pub.publish(mmmm);
-
 }
 
 
@@ -61,18 +39,7 @@ void Test::callbackSimple(const boost::shared_ptr<std_msgs::Float64 const> &in_m
 
 int main(int argc, char **argv){
   ros::init(argc, argv, "Kalman");
-
-  /**
-   * Create object
-   **/
   Test test;
-
-  /**
-   * ros::spin() will enter a loop, servoing callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
   ros::spin();
-
   return 0;
 }
